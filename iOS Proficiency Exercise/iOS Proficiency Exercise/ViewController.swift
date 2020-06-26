40dea9aea6dae4017267b10201ffc462709f71e4
Saipujithreddy <rksaipujithreddy@gmail.com>
2020-06-26 19:23:22 +0530

    InitialCommit for iOSProficiencyExercise

---
+++ iOS Proficiency Exercise/iOS Proficiency Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Proficiency Exercise
+//
+//  Created by Saipujith on 26/06/20.
+//  Copyright © 2020 Saipujith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS Proficiency Exercise/iOS Proficiency ExerciseTests/iOS_Proficiency_ExerciseTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_Proficiency_ExerciseTests.swift
+//  iOS Proficiency ExerciseTests
+//
+//  Created by Saipujith on 26/06/20.
+//  Copyright © 2020 Saipujith. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Proficiency_Exercise
+
+class iOS_Proficiency_ExerciseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
