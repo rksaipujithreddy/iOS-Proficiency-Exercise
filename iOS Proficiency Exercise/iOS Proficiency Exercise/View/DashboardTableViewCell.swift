63f4bdb8e5ffdd2d91add10d5aa91e758ee3f468
Saipujithreddy <rksaipujithreddy@gmail.com>
2020-06-28 19:52:15 +0530

    UiUpdates

--- iOS Proficiency Exercise/iOS Proficiency Exercise/View/DashboardTableViewCell.swift
+++ iOS Proficiency Exercise/iOS Proficiency Exercise/View/DashboardTableViewCell.swift
@@ -11,23 +11,22 @@ import UIKit
 class DashboardTableViewCell: UITableViewCell {
     
     var imageWidthAnchor:NSLayoutConstraint!
-    var imageHeightAnchor:NSLayoutConstraint!
-
-    let viewCellBg: UIView = {
+    
+     lazy var viewCellBg: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = UIColor.white
+        view.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
         view.clipsToBounds = true
         view.layer.shadowColor = UIColor.black.cgColor
         view.layer.shadowOpacity = 1
         view.layer.shadowOffset = CGSize.zero
         view.layer.shadowRadius = 1
-        view.layer.cornerRadius = 1
+        view.layer.cornerRadius = 10
         view.layer.borderColor = UIColor.lightGray.cgColor
         view.layer.borderWidth = 1
         return view
     }()
-    let lblTitle: UILabel = {
+     lazy var lblTitle: UILabel = {
         let label = UILabel()
         label.font = UIFont.boldSystemFont(ofSize: 15)
         label.lineBreakMode = .byWordWrapping // or NSLineBreakMode.ByWordWrapping
@@ -38,14 +37,15 @@ class DashboardTableViewCell: UITableViewCell {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    let imgDisplay: UIImageView = {
+     lazy var imgDisplay: UIImageView = {
         let img = UIImageView()
-        img.contentMode = .scaleAspectFit // without this your image will shrink and looks ugly
+        img.backgroundColor = UIColor.black
+        img.contentMode = .scaleToFill // without this your image will shrink and looks ugly
         img.translatesAutoresizingMaskIntoConstraints = false
         img.clipsToBounds = true
         return img
     }()
-    let lblDescription: UILabel = {
+     lazy var lblDescription: UILabel = {
         let label = UILabel()
         label.lineBreakMode = .byWordWrapping // or NSLineBreakMode.ByWordWrapping
         label.numberOfLines = 0
@@ -61,40 +61,44 @@ class DashboardTableViewCell: UITableViewCell {
         self.contentView.addSubview(viewCellBg)
         
         NSLayoutConstraint.activate([
-        viewCellBg.topAnchor.constraint(equalTo:self.contentView.topAnchor,constant: 8),
-        viewCellBg.leadingAnchor.constraint(equalTo:self.contentView.leadingAnchor,constant: 8),
-        viewCellBg.trailingAnchor.constraint(equalTo:self.contentView.trailingAnchor,constant: -8),
-        viewCellBg.bottomAnchor.constraint(equalTo:self.contentView.bottomAnchor,constant: -8)
-            ])
+            
+            viewCellBg.topAnchor.constraint(equalTo:self.contentView.topAnchor,constant: 8),
+            viewCellBg.leadingAnchor.constraint(equalTo:self.contentView.leadingAnchor,constant: 8),
+            viewCellBg.trailingAnchor.constraint(equalTo:self.contentView.trailingAnchor,constant: -8),
+            viewCellBg.bottomAnchor.constraint(equalTo:self.contentView.bottomAnchor,constant: -8)
+        ])
         
-            viewCellBg.addSubview(imgDisplay)
-                NSLayoutConstraint.activate([
-                imgDisplay.topAnchor.constraint(equalTo:viewCellBg.topAnchor),
-                imgDisplay.leadingAnchor.constraint(equalTo:viewCellBg.leadingAnchor),
-                ])
-        imageWidthAnchor = imgDisplay.widthAnchor.constraint(equalToConstant:100)
-        imageWidthAnchor.isActive = true
-        imageHeightAnchor = imgDisplay.heightAnchor.constraint(equalToConstant:100)
-        imageHeightAnchor.isActive = true
+        let constantWH = (self.contentView.frame.size.width/4)
         
         
+        viewCellBg.addSubview(imgDisplay)
         viewCellBg.addSubview(lblTitle)
-        NSLayoutConstraint.activate([
-        lblTitle.topAnchor.constraint(equalTo:viewCellBg.topAnchor),
-        lblTitle.leadingAnchor.constraint(equalTo:imgDisplay.trailingAnchor),
-        lblTitle.trailingAnchor.constraint(equalTo:viewCellBg.trailingAnchor),
-        lblTitle.heightAnchor.constraint(equalToConstant:100)
-            ])
-        
-
         viewCellBg.addSubview(lblDescription)
         NSLayoutConstraint.activate([
-        lblDescription.topAnchor.constraint(equalTo:lblTitle.bottomAnchor,constant: 0),
-    lblDescription.leadingAnchor.constraint(equalTo:viewCellBg.leadingAnchor,constant: 8),
-        lblDescription.trailingAnchor.constraint(equalTo:viewCellBg.trailingAnchor,constant: -8),
-        lblDescription.bottomAnchor.constraint(equalTo:viewCellBg.bottomAnchor,constant: -8)
+            
+            imgDisplay.heightAnchor.constraint(equalToConstant: constantWH),
+            imgDisplay.leftAnchor.constraint(equalTo: viewCellBg.leftAnchor, constant:20 ),
+            imgDisplay.centerYAnchor.constraint(equalTo:lblTitle.centerYAnchor)
         ])
-
+        imageWidthAnchor =  imgDisplay.widthAnchor.constraint(equalToConstant: constantWH)
+        imageWidthAnchor.isActive = true
+        
+        NSLayoutConstraint.activate([
+            lblTitle.leadingAnchor.constraint(equalTo:imgDisplay.trailingAnchor,constant: 8),
+            lblTitle.topAnchor.constraint(equalTo:viewCellBg.topAnchor,constant: 10),
+            lblTitle.trailingAnchor.constraint(equalTo:viewCellBg.trailingAnchor),
+            lblTitle.heightAnchor.constraint(equalToConstant:constantWH)
+        ])
+        
+        
+        
+        NSLayoutConstraint.activate([
+            lblDescription.topAnchor.constraint(equalTo:lblTitle.bottomAnchor,constant: 20),
+            lblDescription.leadingAnchor.constraint(equalTo:viewCellBg.leadingAnchor,constant: 8),
+            lblDescription.trailingAnchor.constraint(equalTo:viewCellBg.trailingAnchor,constant: -8),
+            lblDescription.bottomAnchor.constraint(equalTo:viewCellBg.bottomAnchor,constant:-8)
+        ])
+        
     }
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -108,24 +112,13 @@ class DashboardTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     func  displayDataInCell(using viewModel: DashboardViewModel) {
-    lblTitle.text = viewModel.title
-    lblDescription.text = viewModel.description
-  
-    imgDisplay.loadImageUsingCacheWithURLString(viewModel.imageURL, placeHolder: nil) { (bool) in
-        //perform actions if needed
-    }
-        if(viewModel.imageURL.isEmpty){
-            imageWidthAnchor = imgDisplay.widthAnchor.constraint(equalToConstant:0)
-            imageHeightAnchor = imgDisplay.heightAnchor.constraint(equalToConstant:0)
-            imageWidthAnchor.isActive = true
-            imageHeightAnchor.isActive = true
-
-        }else
-        {
-              imageWidthAnchor = imgDisplay.widthAnchor.constraint(equalToConstant:100)
-              imageWidthAnchor.isActive = true
-              imageHeightAnchor = imgDisplay.heightAnchor.constraint(equalToConstant:100)
-              imageHeightAnchor.isActive = true
+        lblTitle.text = viewModel.title
+        lblDescription.text = viewModel.description
+        
+        imgDisplay.loadImageUsingCacheWithURLString(viewModel.imageURL, placeHolder: nil) { (bool) in
+            //perform actions if needed
         }
+        print(imgDisplay.image?.pngData())
+
     }
 }
